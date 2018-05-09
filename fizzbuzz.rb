cf9a9b5e97321ad1270f5a345f95da2fcc01bda7
Theresa Morelli <theresa.l.morelli@gmail.com>
2018-05-09 19:26:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(num)
-  if num % 15 == 0
-    return "FizzBuzz"
-  elsif num % 3 == 0
-    return "Fizz"
-  elsif num % 5 == 0
-    return "Buzz"
-  else
-    return nil
+  # if num % 15 == 0
+  #   return "FizzBuzz"
+  # elsif num % 3 == 0
+  #   return "Fizz"
+  # elsif num % 5 == 0
+  #   return "Buzz"
+  # else
+  #   return nil
 end
